6d18972aebe982c6e21a5afeed7ad95a9dda74cd
Sherry Zhang <zsherry168@gmail.com>
2023-03-26 13:19:58 -0400

    Reformatted spacing

--- static/room.js
+++ static/room.js
@@ -1,34 +1,34 @@
 var socketio = io();
 
-    const messages = document.getElementById("messages");
+const messages = document.getElementById("messages");
 
-    const createMessage = (name, msg) => {
+const createMessage = (name, msg) => {
     
-        const content = `
-        <div class="text">
-            <span>
-                <strong>${name}</strong>: ${msg}
-            </span>
-            <span class="muted">
-                ${new Date().toLocaleString()}
-            </span>
-            <i onClick="myFunction(this)" class="fa fa-thumbs-up"></i>
-        </div>
-        `;
-        messages.innerHTML += content;
-    };
+    const content = `
+    <div class="text">
+        <span>
+            <strong>${name}</strong>: ${msg}
+        </span>
+        <span class="muted">
+            ${new Date().toLocaleString()}
+        </span>
+        <i onClick="myFunction(this)" class="fa fa-thumbs-up"></i>
+    </div>
+    `;
+    messages.innerHTML += content;
+};
 
-    socketio.on("message", (data) => {
-        createMessage(data.name, data.message);
-    });
+socketio.on("message", (data) => {
+    createMessage(data.name, data.message);
+});
 
-    const sendMessage = () => {
-        const message = document.getElementById("message");
-        if (message.value == "") return;
-        socketio.emit("message", { data: message.value });
-        message.value = "";
+const sendMessage = () => {
+    const message = document.getElementById("message");
+    if (message.value == "") return;
+    socketio.emit("message", { data: message.value });
+    message.value = "";
 };
 
 function myFunction(x) {
     x.classList.toggle("fa-thumbs-down");
-}
+};
