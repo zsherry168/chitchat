350ada1d0ce0efbca18c4ce83db8fda43be19e37
Sherry Zhang <zsherry168@gmail.com>
2023-03-26 12:54:54 -0400

    Moved JavaScript instructions for room to individual .js file in static

---
+++ static/room.js
@@ -0,0 +1,34 @@
+var socketio = io();
+
+    const messages = document.getElementById("messages");
+
+    const createMessage = (name, msg) => {
+    
+        const content = `
+        <div class="text">
+            <span>
+                <strong>${name}</strong>: ${msg}
+            </span>
+            <span class="muted">
+                ${new Date().toLocaleString()}
+            </span>
+            <i onClick="myFunction(this)" class="fa fa-thumbs-up"></i>
+        </div>
+        `;
+        messages.innerHTML += content;
+    };
+
+    socketio.on("message", (data) => {
+        createMessage(data.name, data.message);
+    });
+
+    const sendMessage = () => {
+        const message = document.getElementById("message");
+        if (message.value == "") return;
+        socketio.emit("message", { data: message.value });
+        message.value = "";
+};
+
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+}
